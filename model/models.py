c662ebaaf5bfb67bd6dcf661178d7658d3f8ab05
hoang loi <loinh20406c@st.uel.edu.vn>
2023-02-09 13:28:10 +0700

    fix

--- model/models.py
+++ model/models.py
@@ -2,11 +2,6 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-
-
-
-
-
 def conv3x3(in_planes, out_planes, stride =1):
   return nn.Conv2D(in_planes, out_planes, stride =stride, kernel_size= 3 ,padding =1)
   
